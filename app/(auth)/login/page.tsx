f980c5a4cd06fbe7241bcb55813e2b5e3ef7beee
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-04-13 22:35:49 +0900

    Update: login.page.tsx

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,29 @@
+import Logo from "@/assets/images/logo.png";
+import Button from "@/components/common/button";
+import Image from "next/image";
+import LoginStyles from "@/styles/login.module.css";
+
+export default function Auth() {
+    return (
+        <div className={LoginStyles.login}>
+            <div className={LoginStyles.logo_area}>
+                <div className={LoginStyles.logo}>
+                    <Image src={Logo} width={100} height={100} alt="로고 이미지"></Image>
+                    <span className={LoginStyles.text}>트리어리</span>
+                </div>
+            </div>
+
+            <div className={LoginStyles.button_wrapper}>
+                <Button size="md" type="outlined">
+                    카카오톡으로 시작하기
+                </Button>
+                <Button size="md" type="outlined">
+                    네이버로 시작하기
+                </Button>
+                <Button size="md" type="outlined">
+                    Google로 시작하기
+                </Button>
+            </div>
+        </div>
+    );
+}
