2ca17af30175fdbaea7e4bfebb25a1815a500ac7
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-04-13 21:46:29 +0900

    Update:  nav-drawer에 avatar 적용

--- components/nav-drawer.tsx
+++ components/nav-drawer.tsx
@@ -3,6 +3,7 @@ import NavStyles from "@/styles/nav-drawer.module.css";
 import Link from "next/link";
 import Icon from "./common/icon";
 import { IconDiary, IconKey } from "@/assets/svg/index";
+import { RadixAvatar } from "./common/avatar";
 
 export default function NavDrawer({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
     useLockBodyScroll(isOpen);
@@ -10,33 +11,33 @@ export default function NavDrawer({ isOpen, onClose }: { isOpen: boolean; onClos
     return (
         <>
             <nav className={`${NavStyles.nav} ${isOpen && NavStyles.visible}`}>
-                <div className={NavStyles.top}>
-                    <ul>
-                        <li onClick={onClose}>
-                            <Link href="/diary">
-                                <Icon size={22}>
-                                    <IconDiary />
-                                </Icon>
-                                다이어리
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className={NavStyles.bottom}>
-                    <ul>
-                        <li onClick={onClose}>
-                            <Link href="/settings/ai">
-                                <Icon size={22}>
-                                    <IconKey />
-                                </Icon>
-                                AI API Key
-                            </Link>
-                        </li>
-                        <li onClick={onClose}>
-                            <Link href="/mypage">kk</Link>
-                        </li>
-                    </ul>
-                </div>
+                <ul className={NavStyles.top}>
+                    <li onClick={onClose}>
+                        <Link href="/diary">
+                            <Icon size={22}>
+                                <IconDiary />
+                            </Icon>
+                            다이어리
+                        </Link>
+                    </li>
+                </ul>
+
+                <ul className={NavStyles.bottom}>
+                    <li onClick={onClose}>
+                        <Link href="/settings/ai">
+                            <Icon size={22}>
+                                <IconKey />
+                            </Icon>
+                            AI API Key
+                        </Link>
+                    </li>
+                    <li onClick={onClose}>
+                        <Link href="/mypage">
+                            <RadixAvatar name="user" />
+                            userName
+                        </Link>
+                    </li>
+                </ul>
             </nav>
             <div
                 className={`${NavStyles.overlay} ${isOpen && NavStyles.visible}`}
